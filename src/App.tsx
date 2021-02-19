7942e0775713cb9678292ed79cf9b7ec069d4bb8
Remato <renatobcostaa@gmail.com>
2021-02-19 18:35:28 -0300

    initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import 'react-native-gesture-handler';
 
-import { View } from 'react-native';
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor="#130525" />
+      <View style={{ flex: 1, backgroundColor: '#130525' }}>
+        <Routes />
+      </View>
+    </NavigationContainer>
+  );
 };
 
 export default App;
